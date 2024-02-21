3f68582fdc87d6dbc3cdb4421aa03a395ddd90a8
2is4 <tesfa.berhanu.bedane@gmail.com>
2024-02-21 15:19:32 +0200

    Add files via upload

---
+++ ssh_test.py
@@ -0,0 +1,41 @@
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+from datetime import datetime, timedelta
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.trigger_rule import TriggerRule
+from datetime import datetime, timedelta
+
+
+ssh_conn_id = 'ssh_demo'
+ssh_command = "echo 'Hello from remote server'"
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5),
+    'start_date': datetime(2024, 1, 1),
+}
+
+
+dag = DAG(
+   dag_id='SSH_DEMO',
+   #(optional but highly recommended)
+   default_args=default_args,
+   description='A simple DAG',
+   schedule_interval='@daily',
+   start_date=datetime(2024, 1, 1),
+)
+
+
+ssh_task = SSHOperator(
+    task_id='ssh_task',
+    ssh_conn_id=ssh_conn_id,
+    command='echo "Hello, world!" > myfile.txt',
+    retries=3,  # Retries for SSH task
+    dag=dag,
+)
